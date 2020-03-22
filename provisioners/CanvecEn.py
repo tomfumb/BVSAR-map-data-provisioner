0c24dd9da59ec73ab45609c5ca79ffe542cf3d1f
Tom Christian <thomaschristian@gmail.com>
2020-03-22 10:48:42 -0700

    Further work calculating image requests for grid

--- provisioners/CanvecEn.py
+++ provisioners/CanvecEn.py
@@ -1,6 +1,9 @@
+import math
 import pyproj
+from functools import reduce
 
 from provisioners.Provisioner import Provisioner
+from provisioners.ImageRequestProperties import ImageRequestProperties
 
 class CanvecEn(Provisioner):
 
@@ -8,18 +11,55 @@ class CanvecEn(Provisioner):
     dpi = 96
     maxW = 4096
     maxH = 4096
-    scales = (250000,150000,70000,35000)
+    scales = (250000, 150000, 70000, 35000)
 
     def __init__(self):
-        self.destCrs = pyproj.Proj("+init=EPSG:3857")
+        self.destCrs = pyproj.Proj('+init=EPSG:3857')
 
     def provision(self, minX, minY, maxX, maxY, outputDirectory):
-        ll = pyproj.transform(self.srcCrs, self.destCrs, minX, minY)
-        ur = pyproj.transform(self.srcCrs, self.destCrs, maxX, maxY)
-        metres = (ur[0] - ll[0], ur[1] - ll[1])
-        inches = (metres[0] / self.metresPerInch, metres[1] / self.metresPerInch)
-        pixelsAtScale = (inches[0] * self.dpi, inches[1] * self.dpi)
-        pixelsAtScales = list(map(lambda scale: list(map(lambda pixelValue: pixelValue / scale, pixelsAtScale)), self.scales))
+        lowerLeft = pyproj.transform(self.srcCrs, self.destCrs, minX, minY)
+        upperRight = pyproj.transform(self.srcCrs, self.destCrs, maxX, maxY)
+        startPoint = list(map(lambda ord: math.floor(ord), lowerLeft))
+        endPoint = list(map(lambda ord: math.ceil(ord), upperRight))
+        totalWidthMetres = endPoint[0] - startPoint[0]
+        totalHeightMetres = endPoint[1] - startPoint[1]
+        imageRequests = dict()
+        for scale in self.scales:
+            imageRequests[scale] = list()
+            remainingWidthMetres = totalWidthMetres
+            remainingHeightMetres = totalHeightMetres
+            remainingHeightPixels = self.metresToPixels(remainingHeightMetres, scale)
+            nextImageHeightPixels = min(remainingHeightPixels, self.maxH)
+            nextImageHeightMetres = math.floor(self.pixelsToMetres(nextImageHeightPixels, scale))
+            lastStartPoint = startPoint.copy()
+            while remainingWidthMetres > 0:
+                remainingWidthPixels = self.metresToPixels(remainingWidthMetres, scale)
+                nextImageWidthPixels = min(remainingWidthPixels, self.maxW)
+                nextImageWidthMetres = self.pixelsToMetres(nextImageWidthPixels, scale)
+                remainingWidthMetres = remainingWidthMetres - nextImageWidthMetres
+                minX, minY = lastStartPoint[0], lastStartPoint[1]
+                maxX, maxY = minX + nextImageWidthMetres, minY + nextImageHeightMetres
+                imageRequests[scale].append([ImageRequestProperties(minX, minY, maxX, maxY, nextImageWidthPixels, nextImageHeightPixels)])
+                lastStartPoint = (maxX, minY)
+            remainingHeightMetres = remainingHeightMetres - self.getTotalRequestedHeight(imageRequests[scale][0])
+            while remainingHeightMetres > 0:
+                baseImageRequest = imageRequests[scale][0][0]
+                remainingHeightPixels = self.metresToPixels(remainingHeightMetres, scale)
+                nextImageHeightPixels = min(remainingHeightPixels, self.maxH)
+                nextImageHeightMetres = self.pixelsToMetres(nextImageHeightPixels, scale)
+                minX, minY = baseImageRequest.minX, baseImageRequest.maxY
+                maxX, maxY = baseImageRequest.maxX, baseImageRequest.maxY + nextImageHeightMetres
+                imageRequests[scale][0].append(ImageRequestProperties(minX, minY, maxX, maxY, baseImageRequest.pixelX, nextImageHeightPixels))
+                remainingHeightMetres = remainingHeightMetres - self.getTotalRequestedHeight(imageRequests[scale][0])
+        print('got to here')
 
-        for idx, scale in enumerate(self.scales):
-            print (str(scale) + ': ' + ','.join(list(map(lambda num: str(num), pixelsAtScales[idx]))))
+    def metresToPixels(self, metres, scale):
+        return (metres / scale) / self.metresPerInch * self.dpi
+        # return list(map(lambda inches: inches * self.dpi, map(lambda metres: (metres / scale) / self.metresPerInch, (x, y))))
+
+    def pixelsToMetres(self, pixels, scale):
+        return ((pixels * scale) / self.dpi) * self.metresPerInch
+        # return list(map(lambda inches: inches * self.metresPerInch, map(lambda pixels: (pixels * scale) / self.dpi, (x, y))))
+
+    def getTotalRequestedHeight(self, imageRequestList):
+        return reduce(lambda total, next: total + next, map(lambda imageRequestProperties: imageRequestProperties.maxY - imageRequestProperties.minY, imageRequestList))
---
+++ provisioners/ImageRequestProperties.py
@@ -0,0 +1,7 @@
+class ImageRequestProperties:
+
+    def __init__(self, minX, minY, maxX, maxY, pixelX, pixelY):
+        self.minX, self.minY, self.maxX, self.maxY, self.pixelX, self.pixelY = minX, minY, maxX, maxY, pixelX, pixelY
+
+    def toString(self):
+        return ' '.join(map(lambda prop: str(prop), (self.minX, self.minY, self.maxX, self.maxY, ' ', self.pixelX, self.pixelY)))
