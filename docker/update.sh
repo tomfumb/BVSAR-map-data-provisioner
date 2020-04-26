57695a1d9c9e8ac032bab8af31ddd7e970ac209b
Tom Christian <thomaschristian@gmail.com>
2020-04-25 21:23:14 -0700

    Further updates to docker setup

--- docker/update.sh
+++ docker/update.sh
@@ -6,13 +6,18 @@ then
       exit 0
 fi
 
-pushd $PROVISIONING_HOME
-pushd ..
+pushd /
 BACKUP_DIR="$PROVISIONING_HOME"_previous
-echo "Removing previous backup if it exists"
-rm -rf cd $BACKUP_DIR
-echo "Backing up current code"
-mv $PROVISIONING_HOME $BACKUP_DIR
+if [ -d "$BACKUP_DIR" ]; then
+      echo "Removing previous backup"
+      rm -rf $BACKUP_DIR
+fi
+CURRENT_FILE_COUNT=`ls -1 $PROVISIONING_HOME/ | wc -l`
+if [ $CURRENT_FILE_COUNT -ne 0 ]; then
+      echo "Backing up current code"
+      mv $PROVISIONING_HOME $BACKUP_DIR
+fi
 echo "Retrieving latest code"
 git clone --depth=1 --branch=master git://github.com/tomfumb/BVSAR-map-data-provisioner $PROVISIONING_HOME
 rm -rf $PROVISIONING_HOME/.git*
+popd
--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if test -z "$PROVISIONING_HOME" 
-then
-      echo "This script should be run inside the Docker container. Set \$PROVISIONING_HOME to a directory location in order to proceed"
-      exit 0
-fi
-
