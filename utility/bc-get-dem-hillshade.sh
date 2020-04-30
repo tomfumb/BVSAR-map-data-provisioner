379d3e6e51f11dec72d69124dc2febd3ab9c80c2
Tom Christian <thomaschristian@gmail.com>
2020-04-30 08:06:18 -0700

    Fixed path issues in utility scripts

--- utility/bc-get-dem-hillshade.sh
+++ utility/bc-get-dem-hillshade.sh
@@ -22,7 +22,7 @@ do
 	rm *.zip
 done
 
-$GDAL_SCRIPTS/gdal_merge.py -n -32767 -o bc-4269.dem `ls -1 *.dem | sed ':a;N;$!ba;s/\n/ /g'`
+python3 $GDAL_SCRIPTS_HOME/scripts/gdal_merge.py -n -32767 -o bc-4269.dem *.dem
 gdalwarp -s_srs EPSG:4269 -t_srs EPSG:3857 -r cubic bc-4269.dem bc-3857.dem
 rm bc-4269.dem
 gdaldem hillshade bc-3857.dem bc-hillshade-225-45.tiff -of GTiff -b 1 -z 1.0 -s 1.0 -az 225 -alt 45
