4d7ad3ab28297d0faf564f6be0644c198221916d
tomfumb <thomaschristian@gmail.com>
2020-11-11 21:16:00 -0800

    Switched to GDAL TMS datasource for PDF export

--- rpi/api/export/export.py
+++ rpi/api/export/export.py
@@ -1,5 +1,4 @@
-from math import log, tan, radians, cos, pi, floor, degrees, atan, sinh
-from gdal import Translate
+from math import log, tan, radians, cos, pi, floor
 
 
 """Adapted from https://github.com/jimutt/tiles-to-tiff"""
@@ -20,44 +19,3 @@ def bbox_to_xyz(lon_min, lon_max, lat_min, lat_max, z):
     x_min, y_max = latlon_to_xyz(lat_min, lon_min, z)
     x_max, y_min = latlon_to_xyz(lat_max, lon_max, z)
     return (floor(x_min), floor(y_min), floor(x_max), floor(y_max))
-
-
-def mercatorToLat(mercatorY):
-    return degrees(atan(sinh(mercatorY)))
-
-
-def y_to_lat_edges(y, z):
-    tile_count = pow(2, z)
-    unit = 1 / tile_count
-    relative_y1 = y * unit
-    relative_y2 = relative_y1 + unit
-    lat1 = mercatorToLat(pi * (1 - 2 * relative_y1))
-    lat2 = mercatorToLat(pi * (1 - 2 * relative_y2))
-    return (lat1, lat2)
-
-
-def x_to_lon_edges(x, z):
-    tile_count = pow(2, z)
-    unit = 360 / tile_count
-    lon1 = -180 + x * unit
-    lon2 = lon1 + unit
-    return (lon1, lon2)
-
-
-def tile_edges(x, y, z):
-    lat1, lat2 = y_to_lat_edges(y, z)
-    lon1, lon2 = x_to_lon_edges(x, z)
-    return [lon1, lat1, lon2, lat2]
-
-
-def georeference_raster_tile(x, y, z, input_path, output_path, expandRgba):
-    bounds = tile_edges(x, y, z)
-    named_args = {
-        "outputSRS": "EPSG:4326",
-        "outputBounds": bounds,
-    }
-    if expandRgba:
-        named_args["rgbExpand"] = "rgba"
-    Translate(
-        output_path, input_path, **named_args,
-    )
