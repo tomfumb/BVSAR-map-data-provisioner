ed34c33e5e746b568bf759f5f250c7e446272a1b
tomfumb <thomaschristian@gmail.com>
2020-11-15 22:12:51 -0800

    Differentiate between SAR Topo local and remote URLs

--- rpi/ui/viewer/src/app/map/tile-urls/tile-urls.component.ts
+++ rpi/ui/viewer/src/app/map/tile-urls/tile-urls.component.ts
@@ -56,7 +56,8 @@ export class TileUrlsComponent {
     return {
       "GIS Kit": `${baseUrl}#Z#/#X#/#Y#.png`,
       "Touch GIS": `${baseUrl}{z}/{x}/{y}.png`,
-      "SAR Topo": `http://localhost/tiles/files/${parameters.tileset.name}/{Z}/{X}/{Y}.png`
+      "SAR Topo (local)": `http://localhost/tiles/files/${parameters.tileset.name}/{Z}/{X}/{Y}.png`,
+      "SAR Topo (remote)": `${baseUrl}{Z}/{X}/{Y}.png`
     };
   }
 }
