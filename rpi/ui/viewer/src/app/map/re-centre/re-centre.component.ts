b1494cd22d4dfb9cd074908ef841687e34be821e
tomfumb <thomaschristian@gmail.com>
2020-11-29 15:47:53 -0800

    Completed lat/long format conversion logic, not yet fully tested

--- rpi/ui/viewer/src/app/map/re-centre/re-centre.component.ts
+++ rpi/ui/viewer/src/app/map/re-centre/re-centre.component.ts
@@ -6,6 +6,19 @@ interface Parameters {
   map: l.map;
 }
 
+type DD = number;
+
+interface DDM {
+  d: number;
+  m: number;
+}
+
+interface DMS {
+  d: number;
+  m: number;
+  s: number;
+}
+
 @Component({
   selector: 'app-re-centre',
   templateUrl: './re-centre.component.html',
@@ -40,18 +53,69 @@ export class ReCentreComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: Parameters
   ) {
     this.map = data.map;
-    const centre = this.map.getCenter()
-    this.latitudeDd = this.roundTo(centre.lat, this.MAX_PRECISION_DD);
-    this.longitudeDd = this.roundTo(centre.lng, this.MAX_PRECISION_DD);
   }
 
   public ngOnInit(): void {
-    this.conversions.fromDecimal();
+    const centre = this.map.getCenter()
+    this.latitudeDd = this.roundTo(centre.lat, this.MAX_PRECISION_DD);
+    this.longitudeDd = this.roundTo(centre.lng, this.MAX_PRECISION_DD);
+    this.ddChanged();
   }
 
   public update(): void {
     this.map.setView(l.latLng(this.latitudeDd, this.longitudeDd), this.map.getZoom());
     this.dialogRef.close();
+  }
+
+  public ddChanged(): void {
+    this.updateDdmFromDd();
+    this.updateDmsFromDd();
+  }
+
+  public ddmChanged(): void {
+    this.latitudeDd = this.roundTo(this.conversions.ddmToDd({
+      d: this.latitudeDdmD,
+      m: this.latitudeDdmM
+    }), this.MAX_PRECISION_DD);
+    this.longitudeDd = this.roundTo(this.conversions.ddmToDd({
+      d: this.longitudeDdmD,
+      m: this.longitudeDdmM
+    }), this.MAX_PRECISION_DD);
+    this.updateDmsFromDd();
+  }
+
+  public dmsChanged(): void {
+    this.latitudeDd = this.roundTo(this.conversions.dmsToDd({
+      d: this.latitudeDmsD,
+      m: this.latitudeDmsM,
+      s: this.latitudeDmsS
+    }), this.MAX_PRECISION_DD);
+    this.longitudeDd = this.roundTo(this.conversions.dmsToDd({
+      d: this.longitudeDmsD,
+      m: this.longitudeDmsM,
+      s: this.longitudeDmsS
+    }), this.MAX_PRECISION_DD);
+    this.updateDdmFromDd();
+  }
+
+  private updateDmsFromDd(): void {
+    const latitudeDms = this.conversions.ddToDms(this.latitudeDd);
+    const longitudeDms = this.conversions.ddToDms(this.longitudeDd);
+    this.latitudeDmsD = latitudeDms.d;
+    this.latitudeDmsM = latitudeDms.m;
+    this.latitudeDmsS = latitudeDms.s;
+    this.longitudeDmsD = longitudeDms.d;
+    this.longitudeDmsM = longitudeDms.m;
+    this.longitudeDmsS = longitudeDms.s;
+  }
+
+  private updateDdmFromDd(): void {
+    const latitudeDdm = this.conversions.ddToDdm(this.latitudeDd);
+    const longitudeDdm = this.conversions.ddToDdm(this.longitudeDd);
+    this.latitudeDdmD = latitudeDdm.d;
+    this.latitudeDdmM = this.roundTo(latitudeDdm.m, this.MAX_PRECISION_DDM);
+    this.longitudeDdmD = longitudeDdm.d;
+    this.longitudeDdmM = this.roundTo(longitudeDdm.m, this.MAX_PRECISION_DDM);
   }
 
   private roundTo(input: number, precision: number): number {
@@ -60,21 +124,28 @@ export class ReCentreComponent implements OnInit {
   }
 
   public readonly conversions = {
-    fromDecimal: () => {
-      const latitudeParts = this.latitudeDd.toString().split(".").map(part => parseInt(part, 10));
-      const longitudeParts = this.longitudeDd.toString().split(".").map(part => parseInt(part, 10));
-      this.latitudeDmsD = latitudeParts[0];
-      this.longitudeDmsD = longitudeParts[0];
-      this.latitudeDdmD = this.latitudeDmsD;
-      this.longitudeDdmD = this.longitudeDmsD;
-      const latitudeMParts = (latitudeParts.length == 2 ? (this.latitudeDd - latitudeParts[0]) * 60 : 0).toString().split(".").map(part => Math.abs(parseInt(part, 10)));
-      const longitudeMParts = (longitudeParts.length == 2 ? (this.longitudeDd - longitudeParts[0]) * 60 : 0).toString().split(".").map(part => Math.abs(parseInt(part, 10)));
-      this.latitudeDmsM = latitudeMParts[0];
-      this.longitudeDmsM = longitudeMParts[0];
-      this.latitudeDdmM = this.roundTo(parseFloat(latitudeMParts.join(".")), this.MAX_PRECISION_DDM);
-      this.longitudeDdmM = this.roundTo(parseFloat(longitudeMParts.join(".")), this.MAX_PRECISION_DDM);
-      this.latitudeDmsS = Math.round(parseFloat(["0",latitudeMParts[1]].join(".")) * 60);
-      this.longitudeDmsS = Math.round(parseFloat(["0",longitudeMParts[1]].join(".")) * 60);
+    ddToDms: (dd: DD): DMS => {
+      const M = this.conversions.ddToM(dd);
+      return {
+          d : 0|dd,
+          m : 0|M/1e7,
+          s : (0|Math.abs(dd)*60%1*6000)/100
+      };
+    },
+    ddToDdm: (dd: DD): DDM => {
+      return {
+          d : 0|dd,
+          m : this.conversions.ddToM(dd)/1e7
+      };
+    },
+    dmsToDd: (dms: DMS): DD => {
+      return dms.d + dms.m/60 + dms.s/(60*60);
+    },
+    ddmToDd: (ddm: DDM): DD => {
+      return ddm.d + ddm.m / 60;
+    },
+    ddToM: (dd: DD): number => {
+      return 0|((dd<0?-dd:dd)%1)*60e7;
     }
   };
 }
