296bebd16df5e2451a1cea8c83b850cbf82a55a8
tomfumb <thomaschristian@gmail.com>
2021-03-03 21:39:57 -0800

    Upped API worker limit and improved loading text

--- rpi/config/web-server/start.sh
+++ rpi/config/web-server/start.sh
@@ -2,4 +2,4 @@
 
 cd `dirname $0`
 
-gunicorn3 -b 0.0.0.0:80 --log-file /www/api/log/main.log --access-logfile /www/api/log/access.log --user=pi --timeout 3600 -w 2 -k uvicorn.workers.UvicornWorker api.main:app
+gunicorn3 -b 0.0.0.0:80 --log-file /www/api/log/main.log --access-logfile /www/api/log/access.log --user=pi --timeout 3600 -w 3 -k uvicorn.workers.UvicornWorker api.main:app
